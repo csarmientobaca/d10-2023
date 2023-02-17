3ea255ca1f4e0422dddc58e42c787a4aa16a8b45
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2023-02-17 17:31:22 +0100

    state going on home, need fix loop

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,74 @@
 // HACER UN FETCH AQUI PARA PROVAR
 import { useEffect, useState } from "react";
 import Hero from "./Hero";
+import { useSelector } from "react-redux"
+import { Button } from "react-bootstrap";
 
 
 
 const Home = () => {
+    const cityContent = useSelector(state => state.city.cityContent)
+    console.log(cityContent)
 
-    const [city, setCity] = useState([]);
+    const [newCordi, setNewCordi] = useState(cityContent)
 
+    setNewCordi(cityContent[0])
+    console.log(newCordi)
+
+    const [city, setCity] = useState(null);
 
     // useEffect(() => {
     //     const fetchData = async () => {
-    //         const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=london&limit=1&appid=afce38265467737eec5cd148f3cabfad`);
+    //         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${newCordi.lat}&lon=${newCordi.lon}&appid=afce38265467737eec5cd148f3cabfad`);
     //         const data = await response.json();
-    //         setCity(data)
+    //         setCity(data, "this is data city")
     //     };
 
     //     fetchData();
-    // }, []);
+    // }, [newCordi]);
+
+    const handleButton = async (e) => {
+
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${newCordi.lat}&lon=${newCordi.lon}&appid=afce38265467737eec5cd148f3cabfad`);
+            if (response.ok) {
+                const data = await response.json()
+                setCity(data)
+                console.log(city)
+
+
+
+
+            } else {
+                alert('Error fetching results')
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
 
     return (
 
         <>
-            {city &&
+            <Button
+                onClick={handleButton}>
+                show
+
+            </Button>
+            <h1>
+                this is home
+            </h1>
+            {/* {city &&
                 <div>
 
                     <h1>home comp;this city is : {city.name}</h1>
-                    <Hero cityContent={city} />
+                    <Hero />
+                    <h2>{city.wheather[0].main}/{city.wheather[0].description}</h2>
+                    <h2>{ }</h2>
+
                 </div>
-            }
+            } */}
 
         </>
 
--- src/components/SearchHome.jsx
+++ src/components/SearchHome.jsx
@@ -1,12 +1,14 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux'
 
 
 
 function BasicExample() {
+    const dispatch = useDispatch()
 
     const [cityName, setCityName] = useState("");
 
@@ -37,7 +39,7 @@ function BasicExample() {
             const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=afce38265467737eec5cd148f3cabfad`)
             if (response.ok) {
                 const data = await response.json()
-                setCordi(data)
+                setCordi(data[0])
             } else {
                 alert('Error fetching results')
             }
@@ -46,15 +48,6 @@ function BasicExample() {
         }
         console.log(cordi)
     }
-
-
-
-
-
-
-
-
-
 
     return (
         <Container>
@@ -88,6 +81,15 @@ function BasicExample() {
                 <Link
                     to="/home"
                     variant="primary"
+                    onClick={() => {
+                        dispatch({
+                            type: "ADD_CITY",
+                            payload: cordi
+                        })
+
+                    }
+
+                    }
                 >
                     Find the cordinates
                 </Link>
